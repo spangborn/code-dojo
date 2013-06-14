b62bdedcda59261f1e213357f20d7b273b449c58
Sawyer Pangborn <sawyer@anesti.org>
2013-06-13 19:46:58 -0600

    Initial Commit

---
+++ jquery-plugin/script/wordcount.jquery.js
@@ -0,0 +1,28 @@
+// wordcount.jquery.js
+
+// jQuery plugin boilerplate
+
+;(function ( $, window, document, undefined ) {
+ 
+ 	// Extend jQuery's function object
+    $.fn.extend({
+
+        "wordCount" : function(options) {
+        	
+        	// Default options object
+            this.defaultOptions = {};
+            // Combine user settings with defaults
+
+            var settings = $.extend({}, this.defaultOptions, options);
+            
+            // Return the object, so chaining works
+           	return this.each(function() {
+                var $this = $(this);
+
+                // Do stuff here!
+            });
+        }
+    });
+
+// Pass in the jQuery, window, and document objects (Why do we not pass in 'undefined'?)
+})(jQuery, window, document);
