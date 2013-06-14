a0a5b56d15a15c86fef89e7e60272d68a869da94
Sawyer Pangborn <sawyer@anesti.org>
2013-06-13 20:19:59 -0600

    Add another sample test

--- jquery-plugin/test/resources/tests.js
+++ jquery-plugin/test/resources/tests.js
@@ -1,4 +1,8 @@
 //tests.js'
 test( "Sample Test", function() {
-  ok( 1 == 1, "Passed!" );
+  ok( 1 == 1, "Check for 1=1" );
+});
+
+test("jQuery Check", function () {
+	ok(typeof jQuery === "function", "jQuery is a function");
 });
