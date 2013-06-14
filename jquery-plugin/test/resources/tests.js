59ce9ca07d3407fcfba2f0d94725101a07804a0c
Sawyer Pangborn <sawyer@anesti.org>
2013-06-13 20:33:50 -0600

    Example test

--- jquery-plugin/test/resources/tests.js
+++ jquery-plugin/test/resources/tests.js
@@ -6,3 +6,23 @@ test( "Sample Test", function() {
 test("jQuery Check", function () {
 	ok(typeof jQuery === "function", "jQuery is a function");
 });
+
+test("Word Count", function () {
+	// Append some test data to the page
+	var $p = $("<p>", {
+		"text"  : "Lorem ipsum dolor sit amet.",
+		"class" : "test-1"
+	}).appendTo("body");
+
+	// Run the plugin
+	$(".test-1").wordCount();
+	var count = $(".test-1").data("words");
+
+	// The actual tests
+	ok(typeof count != undefined, "Count is defined");
+	equal(5, count, "Test 5 words");
+
+	// Tear down
+	$(".test-1").remove();
+
+});
